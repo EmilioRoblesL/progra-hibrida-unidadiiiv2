132de341d13df20184f352998d5c132bccf634f7
Emilio Robles <ejroblesl24@gmail.com>
2024-05-15 06:54:24 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'progra.hibrida.unidadiiiv2',
+  appName: 'progra-hibrida-unidadiiiv2',
+  webDir: 'www'
+};
+
+export default config;
